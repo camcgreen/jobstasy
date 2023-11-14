ca17369fa414f19b648647ae2624a1a78bf43bd3
camcgreen <c.c.green@outlook.com>
2023-11-14 11:58:40 +0000

    Project set-up

--- app/(routes)/jobs/page.tsx
+++ app/(routes)/jobs/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
-import styles from '@/app/page.module.css'
+import styles from '@/app/(routes)/jobs/page.module.css'
 
-export default function Home() {
+export default function Jobs() {
   return (
     <main className={styles.main}>
       <h1>Log in</h1>
