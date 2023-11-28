afdfd6995c8224336f6415e1dbbcc430d0854609
camcgreen <c.c.green@outlook.com>
2023-11-28 09:13:57 +0000

    Fix incorrect name on JobsListContainer

--- app/(routes)/jobs/page.tsx
+++ app/(routes)/jobs/page.tsx
@@ -1,11 +1,11 @@
-import JobsList from '@/app/components/features/jobs/components/JobsList'
+import JobsListContainer from '@/app/components/features/jobs/components/list/jobs-list-container/JobsListContainer'
 import styles from '@/app/(routes)/jobs/page.module.css'
 
 const Jobs: React.FC = () => {
   return (
     <main className={styles.main}>
       <h1>Jobs Hero</h1>
-      <JobsList />
+      <JobsListContainer />
     </main>
   )
 }
